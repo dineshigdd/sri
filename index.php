bd2cefcf4eea9913b9bab142f77d1d29f13cb13d
unknown <dineshigdd@gmail.com>
2022-07-08 07:20:45 -0700

    add code to show post meta

--- index.php
+++ index.php
@@ -5,6 +5,25 @@ get_header();
         while ( have_posts()) : the_post(); ?>
             <article class="post">
                 <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+                <!--  F to denote the month, the jS to indicate the day with a suffix, and Y for the year. -->
+                <p class="post-meta"><?php the_time('F jS, Y'); ?> | 
+                    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID'), get_the_author_meta('user_nicename')); ?>">
+                    <?php the_author();?></a>
+                    |
+                    <?php 
+                        $categories = get_the_category();
+                        $comma = ', ';
+                        $output = '';
+
+                        if( $categories ){
+                            foreach( $categories as $category ){
+                                $output .= '<a href="' . get_category_link( $category-> term_id ). '">'. $category-> cat_name. '</a>' . $comma;
+                            }
+
+                            echo trim( $output, $comma );
+                        }
+                    ?>
+                </p>
                 <?php the_content() ?>
             </article>
         <?php endwhile;    
