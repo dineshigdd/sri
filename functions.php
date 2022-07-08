dfa6dabef1e6c2774288609a506396282d71b956
unknown <dineshigdd@gmail.com>
2022-07-07 22:02:16 -0700

    add child pages, use wp_list_pages with custom fx in function.php,add styles

--- functions.php
+++ functions.php
@@ -13,3 +13,13 @@ function my_theme_enqueue_styles() {
 } 
 
 register_nav_menus( [ 'primary' => __( 'Primary Menu' ) ] ); //Register Menu Location   
+
+function get_the_top_ancestor_id() {
+	global $post;
+	if ( $post->post_parent ) {
+		$ancestors = array_reverse( get_post_ancestors( $post->ID ) );
+		return $ancestors[0];
+	} else {
+		return $post->ID;
+	}
+}
--- page.php
+++ page.php
@@ -6,6 +6,18 @@ if ( have_posts() ) :
 	while ( have_posts() ) : the_post(); ?>
 
         <article class="page-layout">
+			<nav class="child-navigation-menu"> 
+				<ul>
+				<?php 
+					$args = [
+						'child_of' => get_the_top_ancestor_id(),
+						'title_li' => '',
+					];
+
+					wp_list_pages( $args ); //displays a list of pages from your site in list format
+				?>
+				</ul>
+			</nav>
             <h2><?php the_title() ?></h2>
 			<?php the_content() ?>
         </article>
